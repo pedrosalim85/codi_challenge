8907ab8617b2497a6af176830f79fc320c446325
Pedro Salim <pedrosalim@gmail.com>
2024-12-09 15:32:10 -0300

    tempo de experiência

---
+++ challenge6.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import os
+import glob
+import numpy as np
+import sqlite3
+
+####### 1. DEFINIR OS CRITÉRIOS DA AVALIAÇÃO - VARIÁVEL RESPOSTA DO ML
+
+def obter_informacoes_vaga():
+    vaga = input('Qual a vaga que a empresa está oferecendo? ')
+    numero_de_vagas = int(input('Quantas vagas são oferecidas? '))
+    localizacao_empresa = input('Qual a cidade da vaga? ')
+    formacao_req = input('Qual a formação exigida para a vaga? ')
+    habilidades_req = input('Quais habilidades são requeridas? Separe por vírgulas. ').replace(" ", "").split(',')
+
+    return vaga, numero_de_vagas, localizacao_empresa, formacao_req, habilidades_req
+
+
+####### 2. PUXAR OS CURRÍCULOS DO BANCO DE DADOS 
+
+DATABASE = 'C:/users/pedro/desktop/codi/challenge/codi_challenge/curriculos.db'
+
+def get_db():
+    db = sqlite3.connect(DATABASE)
+    return db
+
+def ler_curriculos():
+    db = get_db() 
+    query = "SELECT * FROM cv" 
+    df = pd.read_sql_query(query, db) 
+    db.close()
+
+    df = df.dropna()
+
+    return df
+
+
+######## 3. AVALIAÇÃO DOS CANDIDATOS - MODELO DE MACHINE LEARNING SUPERVISIONADO
+
+def avaliar_candidatos(candidatos_df, formacao_req, habilidades_req, localizacao_empresa, vaga):
+    candidatos_df['PONTUACAO'] = 0
+
+    candidatos_df['PONTUACAO'] += candidatos_df['FORMACAO'].str.contains(formacao_req).astype(int) * 10
+
+    for habilidade in habilidades_req:
+        candidatos_df['PONTUACAO'] += candidatos_df['HABILIDADES'].map(lambda x: 10 if habilidade in x else 0)
+        
+    candidatos_df['PONTUACAO'] += (candidatos_df['CIDADE'] == localizacao_empresa).astype(int)*10
+
+    candidatos_df['PONTUACAO'] += candidatos_df['EXPERIENCIA'].str.contains(vaga).astype(int) * 10
+
+    if (str(candidatos_df['EXPERIENCIA']) == vaga or vaga+'a'):
+        candidatos_df['PONTUACAO'] += candidatos_df['ANOS']*1.5
+
+    candidatos_df['PONTUACAO'] += candidatos_df['RESULTADO_TESTE']
+
+    ###### COMPRIMIR A PONTUAÇÃO PARA REDUZIR AS DIFERENÇAS
+
+    candidatos_df['PONTUACAO'].apply(lambda x: candidatos_df['PONTUACAO']/candidatos_df['PONTUACAO'].max())
+
+    return candidatos_df.sort_values(by='PONTUACAO', ascending=False)
+
+######### SISTEMA GERAL COM TODAS AS FUNÇÕES
+
+def sistema_ats():
+    vaga, numero_de_vagas, localizacao_empresa, formacao_req, habilidades_req = obter_informacoes_vaga()
+    
+    candidatos_df = ler_curriculos()
+    print(candidatos_df)    ### MOSTRA TODOS OS CANDIDATOS CADASTRADOS NO DATABASE
+
+    candidatos_avaliados = avaliar_candidatos(candidatos_df, formacao_req, habilidades_req, localizacao_empresa, vaga)
+    
+    print(f'Os candidatos aprovados para a vaga de {vaga} são:')
+    print(candidatos_avaliados[['NOME', 'PONTUACAO']].head(numero_de_vagas))
+
+
+sistema_ats()
+
+
---
+++ main3.py
@@ -0,0 +1,119 @@
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+
+app = Flask(__name__)
+
+DATABASE = 'C:/users/pedro/desktop/codi/challenge/codi_challenge/curriculos.db'
+
+def get_db():
+    db = sqlite3.connect(DATABASE)
+    return db
+
+@app.route('/')
+def index():
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM cv")
+    cv_list = cursor.fetchall()
+    db.close()
+    return render_template('index.html', cv_list=cv_list)
+
+############ BACK END DOS FORMULÁRIOS:
+
+@app.route('/add', methods=['POST'])
+def add():
+    nome = request.form['nome']
+    sobrenome = request.form['sobrenome']
+    idade = request.form['idade']
+    formacao = request.form['formacao']
+    cidade = request.form['cidade']
+    habilidades = request.form['habilidades']
+    experiencia = request.form['experiencia']
+    tempo_de_exp = request.form['tempo_de_exp']
+
+    cv = {
+        'Nome': nome,
+        'Sobrenome': sobrenome,
+        'Idade': idade,
+        'Formacao': formacao,
+        'Cidade': cidade,
+        'Habilidades': habilidades,
+        'Experiencia': experiencia,
+        'Tempo': tempo_de_exp
+    }
+    cv_id = salvar_no_banco(cv)
+    return redirect(url_for('teste', id=cv_id))
+
+################ SALVAR OS DADOS NO DATABASE EM SQL
+
+def salvar_no_banco(cv):
+    db = get_db()
+    cursor = db.cursor()
+    
+    cursor.execute('''
+        INSERT INTO cv (nome, sobrenome, idade, formacao, habilidades, cidade, experiencia, anos, resultado_teste)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    ''', (cv['Nome'], cv['Sobrenome'], cv['Idade'], cv['Formacao'], cv['Habilidades'], cv['Cidade'], cv['Experiencia'], cv['Tempo'], 0))
+    
+    db.commit() 
+    db.close()
+    print('Dados salvos no banco de dados.')
+
+############# PÁGINA DO TESTE
+
+@app.route('/teste')
+def teste():
+    id = request.args.get('id')
+    return render_template('teste.html', id=id)
+
+############# BUSCAR A ÚLTIMA LINHA DA TABELA DO DATABASE (SQL)
+
+def buscar_ultimo_id():
+    db = get_db()
+    cursor = db.cursor()
+    
+    cursor.execute('SELECT MAX(id) FROM cv')
+    ultimo_id = cursor.fetchone()[0]
+    
+    db.close()
+    return ultimo_id
+
+############ BACKEND DO TESTE
+
+@app.route('/respostas', methods=['POST'])
+def respostas():
+    teste_pontos = 0
+    if request.form.get('pergunta1') == 'c':
+        teste_pontos += 2
+    if request.form.get('pergunta2') == 'a':
+        teste_pontos += 2
+    if request.form.get('pergunta3') == 'e':
+        teste_pontos += 2
+    if request.form.get('pergunta4') == 'b':
+        teste_pontos += 2
+    if request.form.get('pergunta5') == 'd':
+        teste_pontos += 2
+
+    id = buscar_ultimo_id()
+    print(f"ID: {id}, Pontos do teste: {teste_pontos}") 
+    atualizar_teste_no_banco(id, teste_pontos)
+    return redirect(url_for('index'))
+
+############ SALVAR O TESTE NO BANCO DE DADOS (SQL)
+
+def atualizar_teste_no_banco(id, teste_pontos):
+    db = get_db()
+    cursor = db.cursor()
+    
+    cursor.execute('''
+        UPDATE cv
+        SET resultado_teste = ?
+        WHERE id = ?
+    ''', (teste_pontos, id))
+    
+    db.commit()
+    db.close()
+    print('Resultado do teste atualizado no banco de dados.')
+
+if __name__ == '__main__':
+    app.run(debug=True)
